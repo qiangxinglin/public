5fb25757e5afc9eb9b64756befc3f756fb443055
Xinglin Qiang <qiangxinglin@users.noreply.github.com>
2021-01-18 14:36:20 +0800

    Update shell_script_mod.sh

--- jd/shell_script_mod.sh
+++ jd/shell_script_mod.sh
@@ -2,4 +2,4 @@
 # perl -i -p0e 's/let run_pins = \[.*?\]/let run_pins = \["jd_543a487b48ed9,jd_7f66376fa1beb,jd_oemXUqdshpnh,zhaosudan2016,jd_CtqOVvfkzuZV,那长,jd_5aa7dedadcfa1"\]/s' /scripts/jd_joy_run.js
 
 echo -e >> /scripts/docker/merged_list_file.sh
-echo "*/8 14-15 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "*/15 14-15 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1" >> /scripts/docker/merged_list_file.sh
