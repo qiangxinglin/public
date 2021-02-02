a3741abde44f693485ea619a08658a625bee79cb
Xinglin Qiang <qiangxinglin@users.noreply.github.com>
2021-02-02 22:52:45 +0800

    Update crontab_list.sh

--- jd/crontab_list.sh
+++ jd/crontab_list.sh
@@ -5,13 +5,13 @@
 0,1 19-21/1 * * * node /scripts/jd_live_redrain2.js >> /scripts/logs/jd_live_redrain2.log 2>&1
 
 # 半点红包雨
-30,31 12-23/1 * * * node /scripts/jd_live_redrain_half.js >> /scripts/logs/jd_live_redrain_half.log 2>&1
+#30,31 12-23/1 * * * node /scripts/jd_live_redrain_half.js >> /scripts/logs/jd_live_redrain_half.log 2>&1
 
 # 年货直播红包雨
 0 0,9,11,13,15,17,19,20,21,23 3,5,20-30/1 1,2 * node /scripts/jd_live_redrain_nian.js >> /scripts/logs/jd_live_redrain_nian.log 2>&1
 
 # 官方号直播红包雨
-0 0,9,11,13,15,17,19,20,21,22,23 * * * node /scripts/jd_live_redrain_offical.js >> /scripts/logs/jd_live_redrain_offical.log 2>&1
+#0 0,9,11,13,15,17,19,20,21,22,23 * * * node /scripts/jd_live_redrain_offical.js >> /scripts/logs/jd_live_redrain_offical.log 2>&1
 
 # 海产新年抽奖
 10 7 * * * node /scripts/jd_sx.js >> /scripts/logs/jd_sx.log 2>&1
